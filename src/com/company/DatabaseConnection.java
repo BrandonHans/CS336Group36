fd9b6a6beda5e9bc82947820eb6e1d169365d993
Brandon Hansen <csbrandonhansen@gmail.com>
2021-10-25 19:37:41 -0400

    original commit

---
+++ src/com/company/DatabaseConnection.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class DatabaseConnection {
+
+    public DatabaseConnection() {
+        connection();
+    }
+
+    public void connection() {
+
+        try {
+
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/cs336group36", "root", "12345");
+
+            //Statement statement = connection.createStatement();
+
+            //ResultSet resultSet = statement.executeQuery("select * from users");
+
+            //while (resultSet.next()) {
+            //System.out.println(resultSet.getString("username"));
+            //}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    //Possibly make some more methods within this class that query/select important tables that we need often??
+}
